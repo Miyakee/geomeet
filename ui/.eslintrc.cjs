fed536aed463c2a044e0f6ccdfb2caf01ca6256e
Chen Tingyu <tingyu.chen@thoughtworks.com>
2025-12-13 20:50:24 +0800

    [TRELLO-001]chore: add eslint in FE
    Refs: https://trello.com/c/JfFgOaVo/1-001chore-project-init

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -1,18 +1,76 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules', 'build'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react-refresh', 'react'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   rules: {
+    // React Refresh
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    // React
+    'react/prop-types': 'off', // TypeScript handles prop validation
+    'react/react-in-jsx-scope': 'off', // Not needed with React 17+
+    'react/display-name': 'off',
+    'react/jsx-uses-react': 'off', // Not needed with React 17+
+    'react/jsx-uses-vars': 'error',
+    // TypeScript
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'warn',
+    // General
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-debugger': 'error',
+    'no-unused-expressions': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-arrow-callback': 'error',
+    'prefer-template': 'error',
+    'no-duplicate-imports': 'error',
+    'no-useless-rename': 'error',
+    // Code Quality
+    'eqeqeq': ['error', 'always', { null: 'ignore' }],
+    'curly': ['error', 'all'],
+    'brace-style': ['error', '1tbs', { allowSingleLine: false }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single', { avoidEscape: true }],
+    'indent': ['error', 2, { SwitchCase: 1 }],
+    'max-len': ['warn', { code: 120, ignoreUrls: true, ignoreStrings: true }],
   },
 }
