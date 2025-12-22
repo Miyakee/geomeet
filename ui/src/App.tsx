377594bfc89ccac64bd397e98ac20e648b71b6cd
Chen Tingyu <tingyu.chen@thoughtworks.com>
2025-12-22 19:38:58 +0800

    [TRELLO-017]fix: navigate to login page when user input invalid url
    Refs: https://trello.com/c/Wg79nY66/17-017bug-fix

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -50,7 +50,7 @@ function App() {
                 </ProtectedRoute>
               }
             />
-            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="*" element={<Navigate to="/login" replace />} />
           </Routes>
         </BrowserRouter>
       </AuthProvider>
